b1f998d60bd427ce371559bb721f56151cbdfbe6
Datta Avijit <dattavijit@gmail.com>
2019-05-09 11:57:00 +0000

    init

---
+++ webapp/control/ProductRating.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/Control"
+	], function(Control){
+		"use strict";
+		return Control.extend( "CustomControl.CustomControl.Control.ProductRating", {
+			metadata: {
+			},
+			init : function( ){},
+			renderer : function( oRm, oControl ) {}
+		}); 
+	} );
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"CustomControl/CustomControl/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
